a8079c022835e259863a431938f35eaff5e71e60
Unknown <susalvat@hotmail.com>
2017-04-10 21:58:57 +0200

    nuevo netbeans

--- FASE 2.0/proyecto NetBeans/HIMEVICO/src/himevico/HIMEVICO.java
+++ FASE 2.0/proyecto NetBeans/HIMEVICO/src/himevico/HIMEVICO.java
@@ -6,12 +6,14 @@
 package himevico;
 
 import static java.lang.ProcessBuilder.Redirect.from;
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import static javax.swing.text.html.HTML.Tag.SELECT;
+import oracle.jdbc.OracleTypes;
 
 /**
  *
@@ -33,8 +35,32 @@ public class HIMEVICO {
             ResultSet resul = sentencia.executeQuery("SELECT nombre from TRABAJADOR");
 
             while (resul.next()) {
-                System.out.print(resul.getString(1));
+                System.out.println(resul.getString(1));
 
+            }
+            resul.close();
+            sentencia.close();
+            conexion.close();
+        } catch (ClassNotFoundException cn) {
+            cn.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Class.forName("java.sql.DriverManager");
+            Connection conexion = DriverManager.getConnection("jdbc:oracle:thin:@10.10.10.9:1521:db12102", "system", "oracle");
+            Statement sentencia = conexion.createStatement();
+
+            CallableStatement sql = conexion.prepareCall("{call LISTACENTROS(?)}");
+
+            sql.registerOutParameter(1, OracleTypes.CURSOR);
+            sql.execute();
+
+            ResultSet resul = (ResultSet) sql.getObject(1);
+
+            while (resul.next()) {
+                System.out.println(resul.getString(1) + " " + resul.getString(2));
             }
             resul.close();
             sentencia.close();
